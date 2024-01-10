50edee50878dbc892dec399d1f94ab8f03ac5db9
MishchenkoArtem <The.Nesh@yandex.ru>
2024-01-10 13:36:24 +0300

    create app

---
+++ src/components/counter.js
@@ -0,0 +1,37 @@
+import { arrayCounter } from './constans';
+import { arrayBox } from './constans';
+import { popup } from './constans';
+import { popupHeading } from './constans';
+import { popupButton } from './constans';
+
+// --- Изменения счетчиков и открытие попапа.
+export function eventListener() {
+    arrayBox.forEach(item => {
+        item.addEventListener('click', (event) => {
+            const element = event.target;
+
+            if (element.classList.contains('playing-field__image')) {
+                arrayCounter[0].textContent++;
+
+                if (arrayCounter[0].textContent === '5') {
+                    popup.classList.add('popup_opened');
+                    popupHeading.textContent = 'Победа';
+                }
+            } else {
+                arrayCounter[1].textContent++;
+
+                if (arrayCounter[1].textContent === '5') {
+                    popup.classList.add('popup_opened');
+                    popupHeading.textContent = 'Поражение';
+                }
+            }
+        });
+    });
+}
+
+// --- Закрытие попапа, сброс счетчиков.
+popupButton.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
+    arrayCounter[0].textContent = 0;
+    arrayCounter[1].textContent = 0;
+});
---
+++ src/components/main.js
@@ -0,0 +1,26 @@
+import { eventListener } from './counter';
+
+// --- Хаотичное отображение изображения на игровом поле
+(() => {
+    const playingFieldBox = document.querySelectorAll('.playing-field__box');
+
+    const value = (array) => {
+        const element = array[Math.floor(Math.random() * array.length)];
+        return element;
+    }
+
+    setInterval(() => {
+        const item = value(playingFieldBox);
+
+        if (!item.classList.contains('playing-field__image')) {
+            playingFieldBox.forEach((element) => {
+                element.classList.remove('playing-field__image');
+            });
+
+            item.classList.add('playing-field__image');
+        }
+    }, 1000);
+
+})();
+
+eventListener();
