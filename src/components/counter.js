ef22c748fef6ea2c33de69a4ee0194399b4eb14c
Mishchenko Artem <The.Nesh@yandex.ru>
2024-04-19 22:30:52 +0300

    counter setting

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,19 +1,30 @@
 import { arrayCounter, arrayBox, popup, popupHeading, popupButton } from './constans';
 
+const testCounter = (element) => {
+    const nums = element.textContent += 1;
+
+    const sum = nums.split('').
+        reduce(function (currentSum, currentNumber) {
+            return Number(currentSum) + Number(currentNumber)
+        }, 0)
+
+        return element.textContent = sum;
+}
+
 // --- Изменения счетчиков и открытие попапа.
 export function eventListener() {
     arrayBox.forEach(item => {
         item.addEventListener('click', () => {
 
             if (item.classList.contains('playing-field__image')) {
-                arrayCounter[0].textContent ++;
+                testCounter(arrayCounter[0]);
 
                 if (arrayCounter[0].textContent === '5') {
                     popup.classList.add('popup_opened');
                     popupHeading.textContent = 'Победа';
                 }
             } else {
-                arrayCounter[1].textContent ++;
+                testCounter(arrayCounter[1]);
 
                 if (arrayCounter[1].textContent === '5') {
                     popup.classList.add('popup_opened');
